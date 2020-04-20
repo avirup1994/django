7d2e9d371839e9b5120fa5a778313fb3b4d9dc5c
avirup1994 <avirupde28@gmail.com>
2020-04-20 09:37:54 +0530

    first commit

---
+++ djangoapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/',views.about),
+    path('',views.home),
+    path('myapp/',include('myapp.urls')),
+]
